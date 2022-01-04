63f03b756448fb32fc385bf9e0751098d0e81fae
Guillaume Ayoub <guillaume@courtbouillon.org>
2022-01-04 15:42:30 +0100

    Add first version of code

---
+++ slight/__init__.py
@@ -0,0 +1,66 @@
+from os import getenv
+from pathlib import Path
+
+from flask import Flask, abort, render_template, send_from_directory, url_for
+from flask_weasyprint import HTML, render_pdf
+
+VERSION = __version__ = '0.0.0'
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html.jinja2', slideshows=paths('slideshows'))
+
+
+@app.route('/view/<name>/')
+def slideshow(name):
+    for root in paths('slideshows'):
+        if root.name != name:
+            continue
+        slideshow = (root / 'presentation.html').read_text()
+        meta = (root / 'meta.html').read_text()
+        return render_template(
+            'slideshow.html.jinja2', slideshow=slideshow, meta=meta, name=name)
+    return abort(404)
+
+
+@app.route('/view/<name>/static/<path:path>')
+def slideshow_static(name, path):
+    return custom_static('slideshows', name, Path('static') / path)
+
+
+@app.route('/themes/<name>/<path:path>')
+def theme_static(name, path):
+    return custom_static('themes', name, Path(path))
+
+
+@app.route('/pdf/<name>.pdf')
+def print_slideshow(name):
+    html = HTML(url_for('slideshow', name=name), media_type='weasyprint')
+    return render_pdf(html)
+
+
+def custom_static(variable, name, path):
+    for root in paths(variable):
+        if root.name != name:
+            continue
+        if (root / path).is_file():
+            try:
+                return send_from_directory(root, path)
+            except Exception:
+                break
+    return abort(404)
+
+
+def paths(variable):
+    for path in app.config[variable]:
+        for child in path.iterdir():
+            if child.is_dir() and child.name[0] != '.':
+                yield child
+
+
+for variable in ('slideshows', 'themes'):
+    folders = getenv(f'SLIGHT_{variable.upper()}', '').split(':')
+    app.config[variable] = [Path(folder) for folder in folders if folder]
---
+++ slight/__main__.py
@@ -0,0 +1,24 @@
+from argparse import ArgumentParser
+from pathlib import Path
+
+
+from . import __version__, app
+
+
+def main(argv=None):
+    parser = ArgumentParser(
+        prog='slight', description='A lightweight slideshow editor.')
+    parser.add_argument(
+        '-v', '--version', action='version',
+        version=f'slight version {__version__}',
+        help='print slight’s version number and exit')
+    parser.add_argument(
+        '-s', '--slideshows', action='append',
+        help='folder containing slideshows')
+    parser.add_argument(
+        '-t', '--themes', action='append', help='folder containing themes')
+    args = parser.parse_args(argv)
+    app.config['slideshows'].extend(
+        [Path(folder) for folder in args.slideshows])
+    app.config['themes'].extend([Path(folder) for folder in args.themes])
+    app.run(debug=True)
